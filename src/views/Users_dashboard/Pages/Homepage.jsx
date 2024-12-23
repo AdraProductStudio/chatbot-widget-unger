c8d7178ea6744ef0ab7dd48bbe064caf36ffd39b
AdraProductStudio <adraforyou@gmail.com>
2024-12-23 17:44:07 +0530

    UPDATE : Toyota chatbot widget updation

--- src/views/Users_dashboard/Pages/Homepage.jsx
+++ src/views/Users_dashboard/Pages/Homepage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 
 const Homepage = () => {
-  const {token} = useSelector((state)=>state.commonState)
+  const { token } = useSelector((state) => state.commonState)
 
   return (
     <>
@@ -14,11 +14,19 @@ const Homepage = () => {
         >
         </iframe>
         :
-        <iframe
-          src="https://adraproductstudio.github.io/chatbot-widget-rowley/"
-          sandbox="allow-scripts allow-same-origin"
-          className='chatbot-iframe'
-        ></iframe>
+        token === "d74c985dedcbf9df6834b77e4ad3cb320bb78c241585f725bdb337a9e69ec7d3" ?
+          <iframe
+            src="https://adraproductstudio.github.io/chatbot-widget-toyota/"
+            sandbox="allow-scripts allow-same-origin"
+            className='chatbot-iframe'
+          ></iframe>
+          :
+
+          <iframe
+            src="https://adraproductstudio.github.io/chatbot-widget-rowley/"
+            sandbox="allow-scripts allow-same-origin"
+            className='chatbot-iframe'
+          ></iframe>
       }
     </>
 
