8be3a5ed745a0058bbcae641ed805285b80aaab7
AdraProductStudio <adraforyou@gmail.com>
2025-01-11 09:39:15 +0530

    MODIFICATION : Unger login turned-off in https://chat.modelrocket.ai

--- src/views/Users_dashboard/Pages/Homepage.jsx
+++ src/views/Users_dashboard/Pages/Homepage.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 
 const Homepage = () => {
-  const {token} = useSelector((state)=>state.commonState)
+  const { token } = useSelector((state) => state.commonState)
 
   return (
     <>
-      {token !== "67b8d721e99b87f05d0e7653119ca3a1a0f657a708e05c6b0e6f33a937fd6a93" ?
+      {token === "fe11543f71807a4b5d6302e118ac9c5ba7067cf15bd8da27bc2f2ce093318e14" ?
         <iframe
           src="https://adraproductstudio.github.io/chatbot-widget-unger/"
           sandbox="allow-scripts allow-same-origin"
@@ -14,12 +14,20 @@ const Homepage = () => {
         >
         </iframe>
         :
-        <iframe
-          src="https://adraproductstudio.github.io/chatbot-widget-rowley/"
-          sandbox="allow-scripts allow-same-origin"
-          className='chatbot-iframe'
-        ></iframe>
+        token === "d74c985dedcbf9df6834b77e4ad3cb320bb78c241585f725bdb337a9e69ec7d3" ?
+          <iframe
+            src="https://adraproductstudio.github.io/chatbot-widget-toyota/"
+            sandbox="allow-scripts allow-same-origin"
+            className='chatbot-iframe'
+          ></iframe>
+          :
+          <iframe
+            src="https://adraproductstudio.github.io/chatbot-widget-rowley/"
+            sandbox="allow-scripts allow-same-origin"
+            className='chatbot-iframe'
+          ></iframe>
       }
+
     </>
 
 
